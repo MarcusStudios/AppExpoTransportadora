040a88641fd55ff3e8f343b8dbd0bda99566aab1
Marcus <marcuseduardo846@gmail.com>
2024-09-08 03:21:55 -0300

    z

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,26 +1,37 @@
-import React from "react";
-import {View,Text, StyleSheet,TextInput,TouchableOpacity,Button,useColorScheme, ActivityIndicator} from "react-native";
-import * as Animatable from "react-native-animatable";
-import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
-import { auth } from "../../services/firebaseConfig";
-import {signInWithEmailAndPassword} from "firebase/auth"
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-import {app} from "../../services/firebaseConfig.js";
-import {Ionicons} from "@expo/vector-icons";
-import { AlertDialog, Button, XStack, YStack } from 'tamagui'
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Button,
+  useColorScheme,
+  ActivityIndicator,
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+import { auth } from '../../services/firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { app } from '../../services/firebaseConfig.js';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Signin() {
-  const [userMail, setUserMail] = useState("");
-  const [userPass, setUserPass] = useState("");
-  const [isLoading, setIsLoading] = useState(false); // Adicionado estado de loading
-  const [hidePass, setHidePass] = useState(true);
-  
+const [userMail, setUserMail] = useState('');
+const [userPass, setUserPass] = useState('');
+const [hidePass, setHidePass] = useState(true);
+const [isLoadingLogin, setIsLoadingLogin] = useState(false);
+const [isLoadingRegister, setIsLoadingRegister] = useState(false);
+const [isLoadingForgotPassword, setIsLoadingForgotPassword] = useState(false);
+
+
   const navigation = useNavigation();
 
   const userlogin = () => {
-    setIsLoading(true); // Ativa o indicador de atividade
+    setIsLoadingLogin(true); // Ativa o indicador de atividade para login
     signInWithEmailAndPassword(auth, userMail, userPass)
       .then((userCredential) => {
         const user = userCredential.user;
@@ -33,17 +44,41 @@ export default function Signin() {
         alert(errorMessage);
       })
       .finally(() => {
-        setIsLoading(false); // Desativa o indicador de atividade após o término do login
+        setIsLoadingLogin(false); // Desativa o indicador de atividade após o término do login
       });
   };
 
+  const handleRegister = async () => {
+    setIsLoadingRegister(true);
+    // Simulação de uma chamada de rede
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 700)); // Simula um atraso de 2 segundos
+      navigation.navigate("Cadastro");
+    } catch (error) {
+      alert("Ocorreu um erro durante o cadastro.");
+    } finally {
+      setIsLoadingRegister(false);
+    }
+  };
+  
+  const handleForgotPassword = async () => {
+    setIsLoadingForgotPassword(true);
+    // Simulação de uma chamada de rede
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 700)); // Simula um atraso de 2 segundos
+      navigation.navigate("Senha");
+    } catch (error) {
+      alert("Ocorreu um erro ao tentar recuperar a senha.");
+    } finally {
+      setIsLoadingForgotPassword(false);
+    }
+  };
+  
+  
+
   return (
     <View style={styles.container}>
-      <Animatable.View
-        animation="fadeInLeft"
-        delay={500}
-        style={styles.containerHeader}
-      >
+      <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
         <Text style={styles.message}>Bem-vindo(a)</Text>
       </Animatable.View>
 
@@ -56,10 +91,9 @@ export default function Signin() {
           value={userMail}
           onChangeText={setUserMail}
         />
-        
+
         <Text style={styles.title}>Senha</Text>
         <TextInput
-          
           placeholder="Digite sua senha..."
           style={styles.input}
           value={userPass}
@@ -71,25 +105,27 @@ export default function Signin() {
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.button} onPress={userlogin}>
-          {isLoading ? (
-            <ActivityIndicator size="small"  color="white" />
+          {isLoadingLogin ? (
+            <ActivityIndicator size="small" color="white" />
           ) : (
             <Text style={styles.buttonText}>Acessar</Text>
           )}
         </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate("Cadastro")}
-        >
-          <Text style={styles.buttonText}>Cadastre-se</Text>
+        <TouchableOpacity style={styles.button} onPress={handleRegister}>
+          {isLoadingRegister ? (
+            <ActivityIndicator size="small" color="white" />
+          ) : (
+            <Text style={styles.buttonText}>Cadastrar</Text>
+          )}
         </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate("Senha")}
-        >
-          <Text style={styles.buttonText}>Esqueci Senha</Text>
+        <TouchableOpacity style={styles.button} onPress={handleForgotPassword}>
+          {isLoadingForgotPassword ? (
+            <ActivityIndicator size="small" color="white" />
+          ) : (
+            <Text style={styles.buttonText}>Esqueci Senha</Text>
+          )}
         </TouchableOpacity>
       </Animatable.View>
     </View>
@@ -99,38 +135,35 @@ export default function Signin() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#38a69d",
+    backgroundColor: '#38a69d',
   },
   containerHeader: {
-    marginTop: "14%",
-    marginBottom: "8%",
-    paddingStart: "5%",
-
+    marginTop: '14%',
+    marginBottom: '8%',
+    paddingStart: '5%',
   },
   eyeIcon: {
-    flexDirection: "row-reverse",
-    position: "absolute",
+    flexDirection: 'row-reverse',
+    position: 'absolute',
     right: 20,
     top: 170,
     width: '15%',
     height: '5%',
     justifyContent: 'center',
     alignItems: 'center',
-   
-
   },
   message: {
     fontSize: 30,
-    fontWeight: "bold",
-    color: "white",
+    fontWeight: 'bold',
+    color: 'white',
   },
   containerForm: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     flex: 1,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    paddingStart: "5%",
-    paddingEnd: "5%",
+    paddingStart: '5%',
+    paddingEnd: '5%',
   },
   title: {
     fontSize: 20,
@@ -141,23 +174,21 @@ const styles = StyleSheet.create({
     height: 40,
     marginBottom: 12,
     fontSize: 16,
-    width: "100%",
-
-
+    width: '100%',
   },
   button: {
-    backgroundColor: "#38a69d",
-    width: "100%",
+    backgroundColor: '#38a69d',
+    width: '100%',
     borderRadius: 4,
     paddingVertical: 8,
     marginTop: 14,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   buttonText: {
-    color: "white",
+    color: 'white',
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
 });
-0
+0;
